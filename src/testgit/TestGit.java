201ec75bc8956c64062f415d7408d9641b0c468c
ondrej donat <ondrej.donat@tul.cz>
2022-10-19 10:37:43 +0100

    Add hello word line

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -14,6 +14,7 @@ public class TestGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("hellogit");
         // TODO code application logic here
     }
     
